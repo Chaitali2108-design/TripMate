3527a03a204be85296180b153d7d8ee626e23f30
Chaitali Chandele <chaitalichandele2108@gmail.com>
2026-09-05 17:08:52 +0530

    created a server file for backend basic setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("TripMate Backend is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
